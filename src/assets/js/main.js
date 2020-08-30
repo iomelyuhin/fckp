cac872098f76f0d377e1713defe5a0125b9266e9
Ilya Omelyukhin <i.omelyuhin@hmail.com>
2020-08-30 20:59:29 +0300

    finish header for desktops

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,43 @@
-console.log(`main.js is running`)
+document.addEventListener(`DOMContentLoaded`, function () {
+  //!=================================
+  //!==========navigation=============
+  //!=================================
+
+  const navButtons = document.querySelectorAll(".js__navBtn");
+  const navList = document.querySelectorAll(".js__navList");
+
+  navButtons.forEach((navButton) => {
+    navButton.addEventListener("click", (e) => {
+			closeSubmenu()
+      let currentList = e.target.nextSibling;
+      currentList.classList.toggle("active");
+			navButton.classList.toggle("active");
+			docListen()
+    });
+	});
+	
+	function docListen() {
+		
+		document.addEventListener("click", e => {
+			if (
+				e.target.classList != "navigation__item-subitem" &&
+				e.target.classList != "navigation__item-subitem-link" &&
+				e.target.classList != "navigation__item" &&
+				e.target.classList != "navigation__item-btn js__navBtn active"
+			) {
+				closeSubmenu()
+			}
+			
+		})
+	}
+
+
+  function closeSubmenu() {
+    navButtons.forEach((navButton) => {
+      navButton.classList.remove("active");
+    });
+    navList.forEach((list) => {
+      list.classList.remove("active");
+    });
+  }
+});
